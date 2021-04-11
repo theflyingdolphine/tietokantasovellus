9b4a584faceca64d613d3df979ad0647b072b9f7
theflyingdolphine <71017678+theflyingdolphine@users.noreply.github.com>
2021-04-11 15:52:56 +0300

    Add files via upload

---
+++ schema.sql
@@ -0,0 +1,36 @@
+CREATE TABLE users (
+	id SERIAL PRIMARY KEY, 
+	username TEXT, 
+	password TEXT,
+	role INT
+);
+
+CREATE TABLE game1 (
+	id SERIAL PRIMARY KEY,
+	content TEXT
+);
+
+CREATE TABLE game2 (
+	id SERIAL PRIMARY KEY,
+	content TEXT
+);
+
+CREATE TABLE game3 (
+	id SERIAL PRIMARY KEY,
+	creator INT,
+	question TEXT,
+	answer INT
+);
+
+CREATE TABLE reviews (
+	id SERIAL PRIMARY KEY,
+	content TEXT
+);
+
+CREATE TABLE statistics (
+	id SERIAL PRIMARY KEY,
+	username TEXT,
+	game1 INT,
+	game2 INT,
+	other INT
+);
