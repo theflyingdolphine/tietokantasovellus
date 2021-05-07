652caadecd5ddf966725ed17eb5bc3cd4cad1ed4
theflyingdolphine <71017678+theflyingdolphine@users.noreply.github.com>
2021-05-07 10:27:55 +0300

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -10,11 +10,6 @@ CREATE TABLE game1 (
 	content TEXT
 );
 
-
-INSERT INTO game1 (content) VALUES ('12');
-INSERT INTO game1 (content) VALUES ('6');
-INSERT INTO game1 (content) VALUES ('9');
-
 CREATE TABLE game2 (
 	id SERIAL PRIMARY KEY,
 	content TEXT
@@ -26,12 +21,6 @@ CREATE TABLE game3 (
 	question TEXT,
 	answer INT
 );
-
-INSERT INTO game3 (question, answer) VALUES ('What is 2 to the power of 5', 32);
-
-INSERT INTO game3 (question, answer) VALUES ('What is the product of 13 times 5', 65);
-
-INSERT INTO game3 (question, answer) VALUES ('What is the difference of 352 and 72', 280);
 
 
 CREATE TABLE reviews (
